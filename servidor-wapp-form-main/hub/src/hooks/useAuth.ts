11e79681cf913343b1639cd7cbae0bb50c8cb65b
WorkiDigital <workidigitaloficial@gmail.com>
2026-06-10 20:26:59 -0300

    reorganize folder structure to place project files directly in the root

--- servidor-wapp-form-main/hub/src/hooks/useAuth.ts
+++
@@ -1,7 +0,0 @@
-export function isAuthenticated(): boolean {
-  return !!localStorage.getItem('hub_token');
-}
-
-export function getToken(): string | null {
-  return localStorage.getItem('hub_token');
-}
